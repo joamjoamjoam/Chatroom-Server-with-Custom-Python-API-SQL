2ea59a6610cf63f1947029abe0a188d1d78360d2
joamjoamjoam <trentcalllan@yahoo.com>
2016-04-27 16:48:26 -0700

    testing sql data base generator testdatabase.sql added

---
+++ server/testdatabase.sql
@@ -0,0 +1,42 @@
+DROP TABLE friendlist;
+DROP TABLE chat;
+DROP TABLE message;
+
+CREATE TABLE "friendlist" (
+  "listid" SERIAL PRIMARY KEY
+);
+
+CREATE TABLE "Usr" (
+  "login" TEXT PRIMARY KEY,
+  "passwd" TEXT NOT NULL,
+  "bio" TEXT NOT NULL,
+  "age" INTEGER,
+  "listid" INTEGER NOT NULL
+);
+
+CREATE INDEX "idx_usr__listid" ON "Usr" ("listid");
+
+ALTER TABLE "Usr" ADD CONSTRAINT "fk_usr__listid" FOREIGN KEY ("listid") REFERENCES "friendlist" ("listid");
+
+CREATE TABLE "chat" (
+  "chatid" SERIAL PRIMARY KEY,
+  "senderlogin" TEXT NOT NULL
+);
+
+CREATE INDEX "idx_chat__senderlogin" ON "chat" ("senderlogin");
+
+ALTER TABLE "chat" ADD CONSTRAINT "fk_chat__senderlogin" FOREIGN KEY ("senderlogin") REFERENCES "Usr" ("login");
+
+CREATE TABLE "message" (
+  "msgid" SERIAL PRIMARY KEY,
+  "senderlogin" TEXT NOT NULL,
+  "chatid" INTEGER NOT NULL
+);
+
+CREATE INDEX "idx_message__chatid" ON "message" ("chatid");
+
+CREATE INDEX "idx_message__senderlogin" ON "message" ("senderlogin");
+
+ALTER TABLE "message" ADD CONSTRAINT "fk_message__chatid" FOREIGN KEY ("chatid") REFERENCES "chat" ("chatid");
+
+ALTER TABLE "message" ADD CONSTRAINT "fk_message__senderlogin" FOREIGN KEY ("senderlogin") REFERENCES "Usr" ("login")
