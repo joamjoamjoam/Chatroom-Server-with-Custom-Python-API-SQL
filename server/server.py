67920b79dae497f618a922e7946539fb74e8b308
joamjoamjoam <trentcalllan@yahoo.com>
2016-05-03 12:49:33 -0700

    Implementing API calls

--- server/server.py
+++ server/server.py
@@ -274,8 +274,8 @@ if __name__=='__main__':
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # Bind the socket to the port
-    #server_address = ('0.0.0.0', 7908)
-    server_address = ('192.168.1.72', 7908)
+    server_address = ('0.0.0.0', 7908)
+    #server_address = ('192.168.1.72', 7908)
 
     print >>sys.stderr, 'starting up on %s port %s' % server_address
     sock.bind(server_address)
