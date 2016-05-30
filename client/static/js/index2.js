e31db897045042f0f79a2ccb367cee2dd0a4e9b0
Mugol <wrand001@ucr.edu>
2016-05-30 04:53:51 -0700

    clientWIP.py should talk to server now, added index2 for testing

---
+++ client/static/js/index2.js
@@ -0,0 +1,27 @@
+$(".login_submit").click(function() {
+	var user = $('input[id="login_username"]').val();
+	var pass = $('input[id="login_password"]').val();
+	var result = PythonAPI.login(user, pass);
+	if(result)
+	{
+		$(".login_error").html("Loging In...");
+	}
+	else
+	{
+		$(".login_error").html("Login Failed!");
+	}
+});
+
+$(".register_submit").click(function() {
+	var user = $('input[id="register_username"]').val();
+	var pass = $('input[id="register_password"]').val();
+	var result = PythonAPI.register(user, pass);
+	if(result)
+	{
+		$(".register_error").html("Successfully Registered!");
+	}
+	else
+	{
+		$(".register_error").html("Registration Failed!");
+	}
+});
